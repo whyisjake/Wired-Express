c6d243b0fcde96204cbac733840462d9cefbc925
Jake Spurlock <jake_spurlock@wired.com>
2014-11-01 22:38:17 -0700

    Add moment to the globals.

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.locals.moment = require('moment');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
