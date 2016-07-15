b66d1c5491b0b2d5fc251f059404947e83e9c232
Jake Spurlock <whyisjake@gmail.com>
2016-07-15 11:07:28 -0600

    Let's make this work with v2 API.

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,58 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-var endpoint = 'http://wired.com/wp-json';
+var endpoint = 'http://www.wired.com/wp-json';
 var moment = require('moment');
+var WP = require( 'wpapi' );
+var wp = new WP({ endpoint: endpoint });
 
 router.get( '/', function(req, res) {
-	request(
-		{ url: endpoint + '/posts/', json: true },
-		function( error, response, body ) {
-			res.render( 'index', {
-				posts: body,
-				title: 'Wired',
-				subtitle: 'Get in-depth coverage of current and future trends in technology, and how they are shaping business, entertainment, communications, science, politics, and culture at Wired.com.',
-				page: 0
-			});
+	wp.posts().embed().get(function( err, data ) {
+		if ( err ) {
+			console.log('womp womp');
 		}
-	);
+		res.render( 'index', {
+			posts: data,
+			post: data[0],
+			title: 'WIRED',
+			subtitle: 'Get in-depth coverage of current and future trends in technology, and how they are shaping business, entertainment, communications, science, politics, and culture at WIRED.com.',
+			page: 0,
+			home: true,
+		});
+	});
 });
 
 // Let's add category archives pagination
 router.get( '/category/:slug', function(req, res) {
 	var slug = req.params.slug;
-	request(
-		{ url: endpoint + '/posts/?filter[category_name]=' + slug, json: true },
-		function( error, response, body ) {
-			res.render( 'category', {
-				posts: body,
-				title: 'Wired',
-				slug: slug,
-				page: 1
-			});
+	wp.posts().filter({category_name: slug}).embed().get(function( err, data ) {
+		if ( err ) {
+			console.log('womp womp');
 		}
-	);
+		res.render( 'category', {
+			posts: data,
+			title: 'WIRED',
+			slug: slug,
+			page: 1
+		});
+	});
 });
 
 // Let's add category archives pagination
 router.get( '/category/:slug/page/:num', function(req, res) {
-	var slug = req.params.slug;
 	var num = req.params.num;
-	request(
-		{ url: endpoint + '/posts/?filter[category_name]=' + slug + '&page=' + num, json: true },
-		function( error, response, body ) {
-			res.render( 'category', {
-				posts: body,
-				title: 'Wired',
-				page: num,
-				slug: slug
-			});
+	var slug = req.params.slug;
+	wp.posts().filter({ category_name: slug }).page( num ).embed().get(function( err, data ) {
+		if ( err ) {
+			console.log('womp womp');
 		}
-	);
+		res.render( 'category', {
+			posts: data,
+			title: 'WIRED',
+			slug: slug,
+			page: num
+		});
+	});
 });
 
 // Let's add archive pagination
@@ -59,7 +63,7 @@ router.get( '/page/:num', function(req, res) {
 		function( error, response, body ) {
 			res.render( 'archive', {
 				posts: body,
-				title: 'Wired',
+				title: 'WIRED',
 				page: num
 			});
 		}
@@ -68,17 +72,16 @@ router.get( '/page/:num', function(req, res) {
 
 router.get( '/:year/:month/:slug', function(req, res) {
 	var slug = req.params.slug;
-	request(
-		{ url: endpoint + '/posts/?filter[name]=' + slug, json: true },
-		function( error, response, body ) {
-			res.render( 'single', {
-				post: body[0],
-				title: body[0].title,
-				subtitle: body[0].excerpt,
-				post_time: moment( body[0].date ).format('MMMM Do YYYY, h:mm'),
-				link: body[0].link.replace( 'http://wired.com/', '' )
-			});
+	console.log( slug );
+	wp.posts().name(slug).embed().get(function( err, data ) {
+		console.log( data );
+		if ( err ) {
+			console.log('womp womp');
 		}
-	);
+		res.render( 'single', {
+			post: data[0],
+			title: data[0].title.rendered,
+		});
+	});
 });
 module.exports = router;
